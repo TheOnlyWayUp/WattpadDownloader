099cbcc915eaa2828d05ed44ff5772a4014c3627
TheOnlyWayUp <hi@towu.dev>
2024-06-25 18:11:55 +0000

    feat(frontend): Add notfound page

---
+++ src/frontend/src/routes/notfound/+page.svelte
@@ -0,0 +1,29 @@
+<div>
+  <div class="hero min-h-screen">
+    <div
+      class="hero-content flex-col lg:flex-row-reverse glass p-16 rounded shadow-sm"
+    >
+      <div class="text-center max-w-4xl">
+        <h1 class="font-bold text-3xl">
+          Your story was <span
+            class="text-transparent bg-clip-text bg-gradient-to-r to-red-600 via-orange-600 from-red-700"
+            >Not Found</span
+          >
+        </h1>
+        <div class="py-4 space-y-2">
+          <p class="text-2xl">Please check your Story ID.</p>
+          <p class="text-lg pt-2">
+            You can also join us on <a
+              href="https://discord.gg/P9RHC4KCwd"
+              target="_blank"
+              class="link"
+              data-umami-event="Discord">discord</a
+            >, where we release features early and discuss updates.
+          </p>
+        </div>
+
+        <a href="/" class="btn btn-primary btn-lg mt-5">Go Home</a>
+      </div>
+    </div>
+  </div>
+</div>
