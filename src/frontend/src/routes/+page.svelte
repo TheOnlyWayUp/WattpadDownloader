e1d056de19cee3e5c824afb5e6c6354240fd1939
TheOnlyWayUp <hi@towu.dev>
2024-12-26 15:42:00 +0000

    feat(frontend): lowres snow

--- src/frontend/src/routes/+page.svelte
+++ src/frontend/src/routes/+page.svelte
@@ -95,6 +95,32 @@
           >
             Wattpad Downloader
           </h1>
+          <div
+            role="alert"
+            class="alert bg-green-200 mt-10 break-words max-w-md"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="h-6 w-6 shrink-0 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path>
+            </svg>
+            <div>
+              <p>
+                Donators get access to <span class="font-semibold"
+                  >high-speed PDF Downloads</span
+                >
+              </p>
+              <a href="/donate" class="link">Donate now</a>
+            </div>
+          </div>
           <!-- <div role="alert" class="alert bg-cyan-300 mt-5">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -111,14 +137,15 @@
             </svg>
             <span class="text-lg">Please Donate</span>
           </div> -->
-          <p class="pt-6 text-lg">
-            Download your favourite books with a single click!
+          <p class="pt-6 text-lg max-w-md">
+            Download your favourite books with a single click. Have a great new
+            year!
           </p>
           <ul class="pt-4 list list-inside text-xl">
             <!-- TODO: 'max-lg: hidden' to hide on screen sizes smaller than lg. I'll do this when I figure out how to make this show up _below_ the card on smaller screen sizes. -->
             <!-- <li>12/24 - ⚡ Super-fast Downloads!</li>
             <li>12/24 - 📑 PDF Downloads!</li> -->
-            <li>12/24 - 📂 Improved Performance</li>
+            <li>12/24 - 📂 Less Errors, Throttled Downloads</li>
             <li>11/24 - 🔗 Paste Links!</li>
             <li>11/24 - 📨 Send to Kindle Support!</li>
 
