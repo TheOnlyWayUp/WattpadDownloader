682fb4da898906c583870adbbf9bcbfca2086d9b
TheOnlyWayUp <hi@towu.dev>
2024-12-30 23:15:14 +0000

    frontend: Add new year, free premium text

--- src/frontend/src/routes/+page.svelte
+++ src/frontend/src/routes/+page.svelte
@@ -95,6 +95,30 @@
           >
             Wattpad Downloader
           </h1>
+          <div
+            role="alert"
+            class="alert bg-amber-200 mt-10 break-words max-w-md"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="h-6 w-6 shrink-0 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path>
+            </svg>
+            <div>
+              <p>
+                Hey everyone, have a great new year! You're now on the Donator
+                version for a few days :)
+              </p>
+            </div>
+          </div>
           <!-- <div role="alert" class="alert bg-cyan-300 mt-5">
             <svg
               xmlns="http://www.w3.org/2000/svg"
