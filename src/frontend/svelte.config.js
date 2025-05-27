fa9e673aba17aa9e45cbcee89716810ad7dfbf0f
TheOnlyWayUp <hi@towu.dev>
2025-05-27 20:14:07 +0000

    fix(frontend): Disable strict pre-rendering

--- src/frontend/svelte.config.js
+++ src/frontend/svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
 
-const config = { kit: { adapter: adapter() } };
+const config = { kit: { adapter: adapter({ strict: false }) } };
 
 export default config;
