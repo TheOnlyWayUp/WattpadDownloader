6a9b703f5095516c4c2b71b36b2729ab7bf55325
AaronBenDaniel <144371000+AaronBenDaniel@users.noreply.github.com>
2025-06-05 14:22:12 -0400

    fix(api): Import fetch_image() from correct file

--- src/api/src/create_book/__init__.py
+++ src/api/src/create_book/__init__.py
@@ -4,10 +4,10 @@ from .create_book import (
     fetch_story,
     fetch_story_from_partId,
     fetch_story_content_zip,
-    fetch_image,
     fetch_cookies,
 )
 from .generators import PDFGenerator, EPUBGenerator
 from .exceptions import WattpadError, StoryNotFoundError, PartNotFoundError
 from .utils import generate_clean_part_html, slugify
 from .logs import logger
+from .parser import fetch_image
