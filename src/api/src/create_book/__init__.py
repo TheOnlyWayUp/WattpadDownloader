561de5534d4b5c70eb98dd048939b1cbc64fd96d
Aron BenDaniel <144371000+AaronBenDaniel@users.noreply.github.com>
2026-04-21 17:16:04 -0400

    feat(api): List downloading v4 API

--- src/api/src/create_book/__init__.py
+++ src/api/src/create_book/__init__.py
@@ -5,9 +5,11 @@ from .create_book import (
     fetch_story,
     fetch_story_content_zip,
     fetch_story_from_partId,
+    fetch_list,
 )
 from .exceptions import PartNotFoundError, StoryNotFoundError, WattpadError
 from .generators import EPUBGenerator, PDFGenerator
 from .logs import logger
 from .parser import fetch_image
 from .utils import slugify
+from .models import Story, List
